a732bbf1e6bfcf55f48f9562a163a5804f61eeda
Justin Liu <justin.xin.liu@gmail.com>
2016-02-24 09:53:55 +1100

    A way to do animation playback using Xaml Storyboard.

--- AnimationPlayback_XamlStoryboard/MainPage.xaml.cs
+++ AnimationPlayback_XamlStoryboard/MainPage.xaml.cs
@@ -1,30 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace AnimationPlayback_XamlStoryboard
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+        }
+
+        // The SlideUpAnimation Storyboard contains a number of keyframes that do the following -
+        // * Change the Offset Y of the TopPane;
+        // * Change the Opacity of the TopPane;
+        // * Change the Scale X & Y of the Marker;
+        // * Change the Color of the Marker.
+
+        private void LayoutRoot_ManipulationStarting(object sender, ManipulationStartingRoutedEventArgs e)
+        {
+            // 1. Start and pause the animation immediately.
+            SlideUpAnimation.Begin();
+            SlideUpAnimation.Pause();
+        }
+
+        private void LayoutRoot_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            // 2. Get the % value of how much the finger has moved based on the height of the root Grid.
+            var ratio = Math.Abs(e.Cumulative.Translation.Y) / LayoutRoot.ActualHeight;
+            var currentTime = SlideUpAnimation.Duration.TimeSpan.TotalMilliseconds * ratio;
+
+            // 3. 'Scrub' the animation - This is the Seek method that I think is missing in 
+            // Composition's KeyFrameAnimation & CompositionScopedBatch.
+            SlideUpAnimation.Seek(TimeSpan.FromMilliseconds(currentTime));
+        }
+
+        private void LayoutRoot_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            // 4. Resume the animation to finish it off smoothly.
+            SlideUpAnimation.Resume();
         }
     }
 }
